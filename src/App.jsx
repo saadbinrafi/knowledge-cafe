997511d5fd6dbef4a911239e0ba382ab4f4ee0bb
saadbinrafi <saadzaman3311@gmail.com>
2024-04-07 03:09:59 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <h1 className='text-2xl bg-green-200 font-bold'>Knowledge cafe  </h1>
+      
+      
+    </>
+  )
+}
+
+export default App
